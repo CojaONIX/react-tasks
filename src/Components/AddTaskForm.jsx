0d014ee089b076a99c79272bc95712db34c9fb60
CojaONIX <cojaonix@gmail.com>
2024-04-30 12:26:56 +0200

    17 AddTaskForm react-bootstrap

--- src/Components/AddTaskForm.jsx
+++ src/Components/AddTaskForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
 import {useForm} from "react-hook-form";
+import {Form} from "react-bootstrap";
 
 const AddTaskForm = () => {
     const [open, setOpen] = useState(false);
@@ -29,35 +30,34 @@ const AddTaskForm = () => {
             <div className="col-8">
                 <Collapse in={open}>
                     <div id="add-task-form-collapse">
-                        <form onSubmit={handleSubmit(formSubmitted)}>
-                            <div className="mb-3">
-                                <label htmlFor="title">Title <span className="text-danger">*</span></label>
-                                <input
+                        <Form onSubmit={handleSubmit(formSubmitted)}>
+                            <Form.Group className="mb-3" controlId="title">
+                                <Form.Label>Title <span className="text-danger">* {errors.title && <span>{errors.title.message}</span>}</span></Form.Label>
+                                <Form.Control
                                     {...register("title", {
                                         required: 'Title field is required'
                                     })}
                                    type="text"
-                                   id="title"
-                                   className="form-control mt-2"
                                    autoFocus
                                 />
-                                <p className="text-danger mt-2">{errors.title && <span>{errors.title.message}</span>}</p>
-                            </div>
 
-                            <div className="mb-3">
-                                <label htmlFor="body">Body <span className="text-danger">*</span></label>
-                                <textarea
+                            </Form.Group>
+
+
+                            <Form.Group className="mb-3" controlId="body">
+                                <Form.Label>Body <span className="text-danger">* {errors.body && <span>{errors.body.message}</span>}</span></Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    style={{ height: '210px' }}
                                     {...register("body", {
                                         required: 'Body field is required'
                                     })}
-                                    id="body"
-                                    className="form-control mt-2"
                                 />
-                                <p className="text-danger mt-2">{errors.body && <span>{errors.body.message}</span>}</p>
-                            </div>
 
-                            <button className="btn btn-primary form-control my-2">Save</button>
-                        </form>
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">Save</Button>
+                        </Form>
                     </div>
                 </Collapse>
             </div>
