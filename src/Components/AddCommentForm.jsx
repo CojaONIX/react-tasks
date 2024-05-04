a5fba2f7fdbc7c948520077b4266d673937b2e03
CojaONIX <cojaonix@gmail.com>
2024-05-04 10:36:35 +0200

    18 AddCommentForm component

---
+++ src/Components/AddCommentForm.jsx
@@ -0,0 +1,51 @@
+import {Button, Form} from "react-bootstrap";
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {appDataState} from "../States/appDataState";
+import {useForm} from "react-hook-form";
+
+
+const AddCommentForm = ({taskID}) => {
+
+    const setAppDataState = useSetRecoilState(appDataState);
+    const appData = useRecoilValue(appDataState);
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors}
+    } = useForm();
+
+    const formSubmitted = (data) => {
+        data.id = Date.now();
+        data.author = appData.auth.name;
+
+        const modifiedComments = [...appData.tasks[taskID].comments, data];
+        const modifiedTask = {...appData.tasks[taskID], comments: modifiedComments};
+        const modifiedTasks = appData.tasks.map((task, index) => {
+            return index === taskID ? modifiedTask : task;
+        })
+
+        setAppDataState({...appData, tasks: modifiedTasks});
+        reset();
+    };
+
+
+    return (
+        <Form onSubmit={handleSubmit(formSubmitted)} className="mt-3 p-3">
+            <Form.Group className="mb-3" controlId="text">
+                <Form.Label>@{appData.auth.name} Comment: <span className="text-danger">* {errors.text && errors.text.message}</span></Form.Label>
+                <Form.Control
+                    as="textarea"
+                    {...register("text", {
+                        required: 'Comment field is required'
+                    })}
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">Save</Button>
+        </Form>
+    );
+}
+
+export default AddCommentForm;
--- src/Components/CommentsList.jsx
+++ src/Components/CommentsList.jsx
@@ -1,34 +1,9 @@
-import {Button, CloseButton, Form, ListGroup} from "react-bootstrap";
-import {useForm} from "react-hook-form";
-import {useRecoilValue, useSetRecoilState} from "recoil";
-import {appDataState} from "../States/appDataState";
+
+import {CloseButton, ListGroup} from "react-bootstrap";
+import AddCommentForm from "./AddCommentForm";
 
 
 const CommentsList = ({taskID, comments}) => {
-
-    const setAppDataState = useSetRecoilState(appDataState);
-    const appData = useRecoilValue(appDataState);
-
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: {errors}
-    } = useForm();
-
-    const formSubmitted = (data) => {
-        data.id = Date.now();
-        data.author = appData.auth.name;
-
-        const modifiedComments = [...appData.tasks[taskID].comments, data];
-        const modifiedTask = {...appData.tasks[taskID], comments: modifiedComments};
-        const modifiedTasks = appData.tasks.map((task, index) => {
-            return index === taskID ? modifiedTask : task;
-        })
-
-        setAppDataState({...appData, tasks: modifiedTasks});
-        reset();
-    };
 
     return (
         <>
@@ -47,19 +22,8 @@ const CommentsList = ({taskID, comments}) => {
                 }
             </ListGroup>
 
-            <Form onSubmit={handleSubmit(formSubmitted)} className="mt-3 p-3">
-                <Form.Group className="mb-3" controlId="text">
-                    <Form.Label>@{appData.auth.name} Comment: <span className="text-danger">* {errors.text && errors.text.message}</span></Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        {...register("text", {
-                            required: 'Comment field is required'
-                        })}
-                    />
-                </Form.Group>
+            <AddCommentForm taskID={taskID}/>
 
-                <Button variant="primary" type="submit">Save</Button>
-            </Form>
         </>
     )
 }
